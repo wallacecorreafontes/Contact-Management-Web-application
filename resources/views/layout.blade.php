ac4ec9acc9e38c93d88b1427216d421c16d95f98
Wallace F <ideiacompany@gmail.com>
2025-04-07 14:54:04 -0400

    Create Layout

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+
+    <style type="text/tailwindcss">
+
+    </style>
+</head>
+
+<body>
+    @yield('content')
+</body>
+
+</html>
